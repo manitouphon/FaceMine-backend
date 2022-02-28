ebd26f1da367080578b2fe0d2ea7abc70bade4d3
Matthew Price <matt@Tesla-Model-M.local>
2022-02-28 18:51:12 +0700

    new commit

--- scraper.py
+++ scraper.py
@@ -4,19 +4,38 @@ from facebook_scraper import get_posts
 import csv
 
 tmp=[]
+def doSleep():
+    sleep_time = randrange(30,90)
+    print("Sleeping for ",sleep_time, " seconds.")
+    time.sleep(sleep_time)
 
-f = open('PPPostFBScrape.csv','a', encoding='UTF8')
-append = csv.writer(f) 
-append.writerow(["TITLE","LIKES","SHARES"])
-f.close()
+
+
+for post in get_posts('phnompenhpost', pages=10):
+    print(post['text'])
+
+
+
+
+# Comment these for continous scrapping:
+
+# f = open('PPPostFBScrape.csv','a', encoding='UTF8')
+# append = csv.writer(f) 
+# append.writerow(["TITLE","LIKES","SHARES"])
+# f.close()
     
 
-for post in get_posts('PhnomPenhPost', pages=500, cookies='cookie.txt'):
+# Alternate between these if no need to use your account's cookie:
+
+# for post in get_posts('PhnomPenhPost', pages=500, cookies='cookie.txt'):
+for post in get_posts('PhnomPenhPost', pages=500):
+
+    #sleep to avoid getting temp banned by FB
+    doSleep()
     f = open('PPPostFBScrape.csv','a', encoding='UTF8') #open csv with append moode
     append = csv.writer(f) #make writer var
     tmp=[] #Clear tmp
 
-    time.sleep(randrange(30,60)) #sleep to avoid getting temp banned by FB
 
     post_title = post['post_text']
     likes = post['likes']
