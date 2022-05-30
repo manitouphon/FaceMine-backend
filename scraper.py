4ad4e505deb81e8f5a1c1a640fe6b7dd93010eae
Manitou Phon <manitouphon.official@gmail.com>
2022-05-30 18:46:48 +0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -5,30 +5,29 @@ import csv
 
 tmp=[]
 def doSleep():
-    sleep_time = randrange(30,90)
+    sleep_time = randrange(300,900)
     print("Sleeping for ",sleep_time, " seconds.")
     time.sleep(sleep_time)
 
 
 
-for post in get_posts('phnompenhpost', pages=10):
-    print(post['text'])
+
 
 
 
 
 # Comment these for continous scrapping:
 
-# f = open('PPPostFBScrape.csv','a', encoding='UTF8')
-# append = csv.writer(f) 
-# append.writerow(["TITLE","LIKES","SHARES"])
-# f.close()
+f = open('PPPostFBScrape.csv','a', encoding='UTF8')
+append = csv.writer(f) 
+append.writerow(["TITLE","LIKES","SHARES"])
+f.close()
     
 
 # Alternate between these if no need to use your account's cookie:
 
-# for post in get_posts('PhnomPenhPost', pages=500, cookies='cookie.txt'):
-for post in get_posts('PhnomPenhPost', pages=500):
+for post in get_posts('PhnomPenhPost', pages=1000, cookies='cookie.txt'):
+# for post in get_posts('PhnomPenhPost', pages=500):
 
     #sleep to avoid getting temp banned by FB
     doSleep()
